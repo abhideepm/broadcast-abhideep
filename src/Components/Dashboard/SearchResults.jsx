73fa15db472d0a8bef38e6adef42afb2b30ada8c
Abhideep Maity <abhideepm@gmail.com>
2020-07-19 19:34:22 +0530

    polish

--- src/Components/Dashboard/SearchResults.jsx
+++ src/Components/Dashboard/SearchResults.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import Navbar from '../Header/Navbar'
 import { useSelector } from 'react-redux'
+import Navbar from '../Header/Navbar'
+import { selectCurrentUser } from '../redux/loginSlice'
 import { selectQuery } from '../redux/searchSlice'
 import { selectUser } from '../redux/userSlice'
 
 const SearchResults = () => {
 	const searchQuery = useSelector(selectQuery)
 	const userData = useSelector(selectUser)
+	const currentUser = useSelector(selectCurrentUser)
 	let searchResults = []
+
 	if (searchQuery !== '') {
-		searchResults = userData.filter(data => data.username.includes(searchQuery))
+		searchResults = userData
+			.filter(data => data.username.includes(searchQuery))
+			.filter(data => data.username !== currentUser)
 	}
 
 	return (
